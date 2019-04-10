db6fab323671400a8ac7607e272eee8c4055849e
raydelojeda <49485279+raydelojeda@users.noreply.github.com>
2019-04-10 11:55:17 -0400

    Update conn.php

--- coneccion/conn.php
+++ coneccion/conn.php
@@ -1,20 +1,7 @@
 <?php
      session_start();	
-	$l="postgres";
-	$p="1234567";
+	$l="";
+	$p="";
 	$db = NewADOConnection('pgsql');
  	$db->Connect("localhost",$l,$p,"ipc") or die($db->ErrorMsg());
-/*
-   session_start();
-	
-	
-	$l="postgres";
-	$p="pgsql8.4";
-	$db = NewADOConnection('pgsql');
- 	$db->Connect("192.168.150.20",$l,$p,"ipc") or die($db->ErrorMsg());
-		
-	/*$l="postgres";
-	$p="adminwork";
-	$db = NewADOConnection('pgsql');
- 	$db->Connect("192.168.150.20",$l,$p,"ipc") or die($db->ErrorMsg());*/
 ?>
